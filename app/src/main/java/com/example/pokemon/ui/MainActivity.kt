809cafc57586361424d9fde4278ecf79e20fc7d3
Eng Hadi <hadiabdelwhab@gmail.com>
2023-06-27 15:15:29 +0300

    Dynamic toolbar 2

--- app/src/main/java/com/example/pokemon/ui/MainActivity.kt
+++ app/src/main/java/com/example/pokemon/ui/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.example.pokemon.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pokemon.R
-import com.example.pokemon.adapters.PokemonAdapter
 import com.example.pokemon.databinding.ActivityMainBinding
-import com.example.pokemon.model.Pokemon
 import com.example.pokemon.viewmodel.PokemonViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -35,17 +28,22 @@ class MainActivity() : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
 
+
         pokemonViewModel= ViewModelProvider(this)[PokemonViewModel::class.java]
+        setNavHost()
 
 
+
+
+    }
+    private fun setNavHost(){
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         navController = navHostFragment.navController
         setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
         NavigationUI.setupActionBarWithNavController(this,navController)
-
-
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
